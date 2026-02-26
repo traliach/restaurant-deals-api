a4c7d908a2eded49b15974a73c87724cc9f23b56
traliach <ali.achille.traore@gmail.com>
2026-02-26 17:08:50 -0500

    add Favorite model

---
+++ src/models/Favorite.ts
@@ -0,0 +1,14 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const favoriteSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    dealId: { type: Schema.Types.ObjectId, ref: "Deal", required: true },
+  },
+  { timestamps: true }
+);
+
+favoriteSchema.index({ userId: 1, dealId: 1 }, { unique: true });
+
+export type Favorite = InferSchemaType<typeof favoriteSchema>;
+export const FavoriteModel = model<Favorite>("Favorite", favoriteSchema);
